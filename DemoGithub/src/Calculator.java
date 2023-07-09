1bffcab3daea8bdd2d1742454bbc0ad56af6a586
rawat <rawat@Hp>
2023-07-09 20:21:38 +0530

    First Commit

---
+++ DemoGithub/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator
+{
+	int n1=5, n2=6;
+	public void add() {
+		System.out.println("Sum is" +"n1=n2");
+	}
+
+	
+	public static void main(String[] args) {
+		
+		Calculator calc=new Calculator();
+		calc.add();
+	}
+
+}
